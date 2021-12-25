1e3a2a4d2aa12c32eed187e4705f054041fefa4b
Maira509 <bsef18a509@pucit.edu.pk>
2021-12-25 18:19:04 +0500

    Google Maps implemented

--- app/src/main/java/com/example/womensafety/MainActivity.java
+++ app/src/main/java/com/example/womensafety/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.womensafety;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1;
+      button1 = (Button) findViewById(R.id.button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+    }
+    public void openActivity3() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
